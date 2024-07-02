7ab5ddda806db32ee767e5ac109c5d127cc87126
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-02 19:25:42 +0000

    commit config done

--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -1,1 +1,15 @@
 console.log("Shubham")
+const accountId = 1234
+let accountEmail = "shubhamdoodle.com"
+var accountPassward = "12458"
+accountCity = "Bihar"
+
+// accountId = 2 not allowed 
+
+console.log(accountId);
+accountEmail = "hc.hc.com"
+accountPassward = "3254"
+accountCity = "Mumbai"
+
+
+console.table([accountEmail,accountPassward,accountCity]);

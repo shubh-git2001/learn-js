55805e26827078266cb04576f463d019329368ed
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-06 05:33:59 +0000

    Conversion_of_DataTypes

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,15 +1,16 @@
 // let score = "33abc";
 // let score = null;
+
 let score = undefined
-console.log(typeof score); // Both are Same 
-console.log(typeof (score));
+//console.log(typeof score); // Both are Same 
+//console.log(typeof (score));
 
 /* Vaise score declare "string" mein kiya hai but "valueInNumber" ka use karke "Number" mein change kar sakte hai */
 
-
+// Number Conversion
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // valueInNumber 
 // "33" --> 33
@@ -19,4 +20,20 @@ console.log(valueInNumber);
 // null --> 0
 // undefined --> NaN
 
-   
+
+// Boolean Conversion
+let isLoggedIn = "";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+
+// Boolean Conversion by --> isLoggedIn
+// 1 --> true ; 0 --> false
+// "" --> false ; " " --> true 
+// "shubham" --> true
+
+// String Conversion 
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

aa0dbce584e853eab2f9ce45a2c15336db9dc193
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-06 11:13:36 +0000

    dataType_Summary_config

--- 01_Basic/dataType_Summary.js
+++ 01_Basic/dataType_Summary.js
@@ -4,6 +4,27 @@ Primitive
 String, Number, Boolean, null,undefined, Symbol, BigInt --> scientific value or big values can be store 
 */
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// **************************************************//
+// **************************************************//
+
+
+
 // Reference Type --> Non Primitive 
 
 /* 
@@ -12,3 +33,18 @@ Non Primitive
 Arrays, Objects, Functions
 */
 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3

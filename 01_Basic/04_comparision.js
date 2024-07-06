8a9ccc23236d8b510b35a7cdb7a995637bbe56bc
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-06 11:07:04 +0000

    dataType

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,34 @@
+/*
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+*/
+
+// Ye comparsion theek bcz both side have same dataType
+
+//****************************************************//
+
+
+console.log("02" < 1);
+console.log ("02" > 1);
+// comparison did not good bcz they compare both differnt dataType 
+
+
+// **************************************************//
+
+
+/*
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0);
+*/
+
+// Here also not good comparison bcz both different dataType 
+
+// NOTE => always compare same type of data types 
+// === this is used to compare values as well as dataTypes 
+
+console.log("02" === 1);
+
---
+++ 01_Basic/dataType_Summary.js
@@ -0,0 +1,14 @@
+/* 
+Primitive  
+7 types :- 
+String, Number, Boolean, null,undefined, Symbol, BigInt --> scientific value or big values can be store 
+*/
+
+// Reference Type --> Non Primitive 
+
+/* 
+Non Primitive 
+3 types :-
+Arrays, Objects, Functions
+*/
+

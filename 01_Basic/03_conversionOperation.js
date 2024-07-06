cf50bee61ca2566ea20288f5c4dc3d216a6e7fce
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-06 09:37:24 +0000

    config

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -37,3 +37,32 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************ Operations ************
+// let value = 2;
+// let negValue = -value;
+// console.log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Namaste";
+let str2 = " Duniya"
+
+// add two string 
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log ("1" + 2);
+console.log (1 + "2");
+console.log ("1" + "2" + 2);
+console.log (1 + 2 + "2");
+console.log("2"+1+1);
+
+// first no. is string than all no will string 
+// if first no. is no. than it can be add and proceed next value 
+

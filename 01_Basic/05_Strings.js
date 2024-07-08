6897d8c4b656e59069ba1303196774c53c3d0d4a
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-08 17:35:42 +0000

    String

--- 01_Basic/05_Strings.js
+++ 01_Basic/05_Strings.js
@@ -3,4 +3,10 @@ const age = 22
 
 // console.log(name + age);
 
-console.log('my name is ${name} and my age is ${age}');
+// use backtick for string interpolation 
+// console.log(`my name is ${name} and my age is ${age}`);
+
+const gameName = new String('ShubhamGo');
+console.log(gameName);
+// console.log(typeof gameName);
+// console.log(gameName.toUpperCase());

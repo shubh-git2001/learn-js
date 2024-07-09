5d0b26b846da04923d57cdfee3e8a692aec4d7b2
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-09 10:19:55 +0000

    nums.math_commit

--- 01_Basic/05_Strings.js
+++ 01_Basic/05_Strings.js
@@ -1,5 +1,5 @@
-const name = 'Shubham'
-const age = 22
+// const name = 'Shubham'
+// const age = 22
 
 // console.log(name + age);
 
@@ -10,3 +10,30 @@ const gameName = new String('ShubhamGo');
 console.log(gameName);
 // console.log(typeof gameName);
 // console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
+
+// const newStringOne = "  hitesh  ";
+// console.log(newStringOne);
+
+const a = "a";
+const b = "b";
+if (a < b) {
+  true
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
+
+const str1 = "  Shubham ";
+console.log(str1);
+console.log(str1.trim());
---
+++ 01_Basic/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number (100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.67320;
+console.log(otherNumber.toPrecision(5)); 
+
+const anotherNumber = 100000000;
+console.log(anotherNumber.toLocaleString('en-IN'));
+
+// =======================     Maths ============================ //
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(10.3));
+// console.log(Math.ceil(10.3));
+// console.log(Math.floor(10.3));
+// console.log(Math.max(5,3,8,9,2));
+// console.log(Math.min(5,3,8,9,2));
+// console.log(Math.sqrt(9));
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 25;
+
+console.log(Math.random + )

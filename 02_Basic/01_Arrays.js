2d299c3d296707bfd1b6dd9c33c53a8eb5605403
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-12 16:34:13 +0000

    arrays

--- 02_Basic/01_Arrays.js
+++ 02_Basic/01_Arrays.js
@@ -1,13 +1,15 @@
 //  Array
- const myAarray = [0,2,8,7,3,9];
- // Access through there index 
- console.log (myFirstAarray[0]);
+const myArray = [0,2,8,7,3,9];
+// Access through there index 
+console.log (myArray[0]);
 
 
 //  In Sabhi Tariko se Array assign kar sakte hai 
 const myHeroes = ["Shaktiman", "GoluBoba"];
- const mySecondArray = new Array(1,4,5,6);
- console.log(myArr[0]);
+const mySecondArray = new Array(1,4,5,6);
+console.log(mySecondArray[0]);
 
 //  Array Methods 
-myArray.push = 
+// myArray.push(6)
+// console.log(myArray)
+

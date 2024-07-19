577ac6ea6375ac036c06877d424fde9f12f74cd3
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-19 16:28:03 +0000

    object_Array

--- 02_Basic/03_object.js
+++ 02_Basic/03_object.js
@@ -1,12 +1,54 @@
-//Object 
+// skelton 
+/* 
+Two Methods in an onbject to decalre
+ 1 => Literals 
+ 2 => constructor 
+ 
+ */
+   
 
+//Object literals 
+
+// Decalre Array 
+const mySys = Symbol("Key1")
 const JsUser = {
     name : "Shubham",
     age : 22,
-    email: "shubham888@hotmail.in",
+    email: "shubh@hotmail.in",
     isLoggedIn: "False",
-    LastLogindays: ["Monday", "Saturday"]
+    LastLogindays: ["Monday", "Saturday"],
+
+    [mySys] : "Key1"
     
 }
 
-console.log(JsUser["email"])
+// console.log(JsUser);
+
+// Both method use to access object
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+
+// console.log(JsUser[mySys]);
+
+// Overwrite value jo pehle se decalre hai 
+// JsUser.email = "shubh@chatgpt.com";
+// console.log(JsUser.email)
+
+// JsUser.email = "shubh@chatgpt.com";
+// Object.freeze(JsUser);
+// console.log(JsUser.email);
+
+// Value ko over-write nahi kar payenge 
+// JsUser.email = "Shubham11@krok.com";
+// console.log(JsUser.email);
+
+
+JsUser.greeting = function(){
+    console.log("Hello shubham");
+}
+console.log(JsUser.greeting());
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello bhai, ${this.name}`);
+}
+console.log(JsUser.greetingTwo());

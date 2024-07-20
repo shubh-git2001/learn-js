91c3aa7126161772c80b4bca39384529f2a5681e
SHUBHAM <142477798+shubh-git2001@users.noreply.github.com>
2024-07-20 20:19:33 +0000

    Funtion

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,23 @@
+ function sayMyname () {
+    console.log("S")
+    console.log("A")
+    console.log("L")
+    console.log("M")
+    console.log("A")
+    console.log("N")
+ }
+
+//  sayMyname()
+
+
+
+function addTwoNumber(number1 , number2){
+      let result = console.log(number1 + number2);
+      
+      return result
+      
+}
+
+const result = addTwoNumber(3,5);
+console.log("Result :", result);
+
